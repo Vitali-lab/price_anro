41471f9873912980322b4991f2bb0b2e90fbf341
Vitali <3134114@mail.ru>
2025-09-11 14:56:44 +0300

    Fix React Router basename for GitHub Pages subfolder deployment

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import App from './App.jsx'
 initPWA();
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/price_anro">
   <AuthProvider>
     <App />
   </AuthProvider>
