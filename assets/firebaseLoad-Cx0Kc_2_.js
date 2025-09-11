8110013319954799b896b618071b9b337648eead
Vitali <3134114@mail.ru>
2025-09-11 14:06:07 +0300

    Updates

--- assets/firebaseLoad-Cx0Kc_2_.js
+++
@@ -1,1 +0,0 @@
-import{c as a,a as s,d as t,f as e}from"./firebase-CrVzxzD3.js";import{e as r}from"./index-DALuXi8Z.js";const o=async()=>{const t=a(r,"products");return(await s(t)).docs.map(a=>a.data())},d=async()=>{const a=t(r,"meta","lastUpdated"),s=await e(a);return s.exists()?s.data().date:null};export{o as a,d as f};
