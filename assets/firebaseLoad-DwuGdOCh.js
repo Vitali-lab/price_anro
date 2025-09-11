cba812aba85a2a70a1674eecae96998309330597
Vitali <3134114@mail.ru>
2025-09-11 14:21:15 +0300

    Updates

---
+++ assets/firebaseLoad-DwuGdOCh.js
@@ -0,0 +1,1 @@
+import{c as a,a as s,d as t,f as e}from"./firebase-DDnKwQsl.js";import{e as r}from"./index-D3rtl_IZ.js";const o=async()=>{const t=a(r,"products");return(await s(t)).docs.map(a=>a.data())},d=async()=>{const a=t(r,"meta","lastUpdated"),s=await e(a);return s.exists()?s.data().date:null};export{o as a,d as f};
